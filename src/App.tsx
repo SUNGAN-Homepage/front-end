d7d89ab7ab4a8de7be3e7eadf7205fc429732940
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-01-08 12:32:28 +0900

    feat:컴포넌트 분할

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import Header from './components/Header.tsx';
 import './index.css';
-import { Intro } from './components/Intro.tsx';
+import { Intro } from './components/Intro/Intro.tsx';
+import { DetailIntro } from './components/DetailIntro/DetailIntro.tsx';
+import { PortFolio } from './components/PortFolio/PortFolio.tsx';
+import { Partner } from './components/Partner/Partner.tsx';
+import { Contact } from './components/Contact/Contact.tsx';
+import { Footer } from './components/Footer.tsx';
 function App() {
   return (
     <>
       <Header />
       <Intro />
-
+    <DetailIntro/>
+      <Partner/>
+      <PortFolio/>
+  <Contact/>
+      <Footer/>
     </>
   );
 }
--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,3 +1,3 @@
 export const Intro = () => {
-  return <></>;
+  return <div>Intro</div>;
 };
