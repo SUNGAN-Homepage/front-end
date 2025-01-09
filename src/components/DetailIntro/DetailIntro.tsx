6ec02fc0ffecb2a5cc3a1815f84d39a5a33172a4
hottestpotato <wlsynyjdcks7@gmail.com>
2025-01-09 16:29:09 +0900

    feat: DetailIntro 구현현

--- src/components/DetailIntro/DetailIntro.tsx
+++ src/components/DetailIntro/DetailIntro.tsx
@@ -1,5 +1,57 @@
-export const DetailIntro = () => {
-  return (
-    <div>DetailIntro</div>
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './DetailIntro.css';
+import companyImage1 from '../../assets/PHOTOFRAME.png';
+
+export const DetailIntro: React.FC = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return(
+    // 기업 소개 컴포넌트
+    <section className="detail-intro">
+      <Slider {...settings}>
+      <div className="slide">
+      <div className="slide-content">
+          <div className="image-wrapper">
+              <img src= {companyImage1} alt="소개 이미지 1" />
+            </div>
+            <h2>순간 스튜디오</h2>
+          <p>
+            안녕하세요 순간 스튜디오입니다.
+          </p>
+        </div>
+      </div>
+      <div className="slide">
+      <div className="slide-content">
+          <div className="image-wrapper">
+              <img src= {companyImage1} alt="소개 이미지 1" />
+            </div>
+            <h2>순간 스튜디오2</h2>
+          <p>
+            안녕하세요 순간 스튜디오입니다.
+          </p>
+        </div>
+      </div>
+      <div className="slide">
+      <div className="slide-content">
+          <div className="image-wrapper">
+              <img src= {companyImage1} alt="소개 이미지 1" />
+            </div>
+            <h2>순간 스튜디오3</h2>
+          <p>
+            안녕하세요 순간 스튜디오입니다.
+          </p>
+        </div>
+      </div>
+      </Slider>
+    </section>
   );
 };
