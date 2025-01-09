6a0d44871f739907dfa0fe81c68024d993b1bfd2
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-01-09 15:49:48 +0900

    feat:Footer 기능능

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,18 @@
+import { Box, Typography } from '@mui/material';
+
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box
+      sx={{
+        backgroundColor: '#303030',
+        textAlign: 'center',
+        paddingTop: 1,
+        paddingBottom: 1,
+      }}
+    >
+      <Typography sx={{ color: 'white' }}>전화:xxx-xxxx-xxxx</Typography>
+      <Typography sx={{ color: 'white' }}> 이메일:xxxx@xxxx.xxx</Typography>
+      <Typography sx={{ color: 'white' }}> 위치:xxxxx xxx xxxxx xx</Typography>
+    </Box>
   );
 };
