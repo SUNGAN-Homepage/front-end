3342588a6492c5f3194a653fc3f2ea87ba3ab2be
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-01-09 15:49:29 +0900

    feat:Intro 기능

--- src/components/Intro/Intro.tsx
+++ src/components/Intro/Intro.tsx
@@ -1,3 +1,36 @@
+import { Box, Button, Typography } from '@mui/material';
+
 export const Intro = () => {
-  return <div>Intro</div>;
+  return (
+    //추후 이미지로 대체
+    <Box sx={{ background: 'gray' }}>
+      <Box
+        sx={{
+          textAlign: 'center', // 텍스트 중앙 정렬
+          margin: '0 auto', // Box 자체를 부모 내에서 수평 중앙 정렬 (필요 시)
+          padding: 4, // 패딩 추가 (선택 사항)
+        }}
+      >
+        <Typography variant="h3" marginBottom={5} marginTop={5}>
+          간략 소개
+        </Typography>
+        <Typography variant="h4" marginBottom={5}>
+          간단하게 기업에 대한 소개
+        </Typography>
+
+        <Button
+          sx={{
+            marginTop: 5,
+            width: '150px',
+            background: 'white',
+            color: 'black',
+            // fontSize: '12px',
+            marginBottom: 10,
+          }}
+        >
+          예약하기
+        </Button>
+      </Box>
+    </Box>
+  );
 };
