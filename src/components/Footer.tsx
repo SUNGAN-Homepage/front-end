8b14c87e9fa5831817c24253a1f940166f9ab7c6
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-01-12 15:33:53 +0900

    feat:Footer 반응형 디자인

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,15 +4,34 @@ export const Footer = () => {
   return (
     <Box
       sx={{
+        height: { sm: '100px' },
         backgroundColor: '#303030',
         textAlign: 'center',
         paddingTop: 1,
         paddingBottom: 1,
+        display: 'flex',
+        justifyContent: 'center', // 중앙에 배치
+        alignItems: 'center', // 수직 중앙 정렬
       }}
     >
-      <Typography sx={{ color: 'white' }}>전화:xxx-xxxx-xxxx</Typography>
-      <Typography sx={{ color: 'white' }}> 이메일:xxxx@xxxx.xxx</Typography>
-      <Typography sx={{ color: 'white' }}> 위치:xxxxx xxx xxxxx xx</Typography>
+      <Box
+        sx={{
+          display: { sm: 'flex' },
+          justifyContent: { sm: 'center' }, // 항목들을 가로로 배치
+          gap: { sm: 4 }, // 항목들 간의 간격 설정
+          flexWrap: { sm: 'wrap' }, // 작은 화면에서 항목들이 줄 바뀌도록 설정
+        }}
+      >
+        <Typography sx={{ color: 'white', fontSize: { sm: '20px' } }}>
+          전화: xxx-xxxx-xxxx
+        </Typography>
+        <Typography sx={{ color: 'white', fontSize: { sm: '20px' } }}>
+          이메일: xxxx@xxxx.xxx
+        </Typography>
+        <Typography sx={{ color: 'white', fontSize: { sm: '20px' } }}>
+          위치: xxxxx xxx xxxxx xx
+        </Typography>
+      </Box>
     </Box>
   );
 };
