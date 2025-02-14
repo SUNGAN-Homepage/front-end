a382b6a6210f78d9c0d7d653f78ae99f58cf14ae
hojin535 <123153695+hojin535@users.noreply.github.com>
2025-02-14 20:52:38 +0900

    feat:contact 백엔드 통신 임시 추가

--- src/components/Contact/ContactForm.tsx
+++ src/components/Contact/ContactForm.tsx
@@ -11,12 +11,55 @@ import {
 } from '@mui/material';
 import Button from '@mui/material/Button';
 import { useState } from 'react';
+import axios from 'axios';
 
 export default function ContactForm() {
-  const [inquiryType, setInquiryType] = useState<string>('');
+  const [name, setName] = useState<string>();
+  const [company, setCompany] = useState<string>();
+  const [email, setEmail] = useState<string>();
+  const [phone, setPhone] = useState<string>();
+  const [inquiryType, setInquiryType] = useState<string>();
+  const [text, setText] = useState<string>();
+  console.log(
+    'name:',
+    name,
+    'company:',
+    company,
+    'email:',
+    email,
+    'phone:',
+    phone,
+    'inquiryType:',
+    inquiryType,
+    'text:',
+    text,
+  );
+
   const handleChangeInquiry = (event: SelectChangeEvent) => {
     setInquiryType(event.target.value);
   };
+
+  const handleSubmit = async () => {
+    const data = {
+      name,
+      company: company || 'NA',
+      email,
+      phone: phone || 'NA',
+      inquiryType,
+      text: text || 'NA',
+    };
+    if (!name || !email || !inquiryType) {
+      alert('필수 사항을 입력해 주세요');
+    } else {
+      try {
+        await axios.post('aaasdf', data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        alert('오류 발생');
+      }
+    }
+  };
+
   return (
     <div>
       <Divider
@@ -34,14 +77,45 @@ export default function ContactForm() {
         }}
       />
       <Typography marginTop={2}>1. 이름*</Typography>
-      <TextField fullWidth variant="standard" placeholder="(필수 사항)" />
+      <TextField
+        fullWidth
+        variant="standard"
+        placeholder="(필수 사항)"
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
       <Typography marginTop={2}>2. 상호명 (회사명)</Typography>
-      {/*todo: 상호명하고 연락처는 없으면 "NA"로 보내기*/}
-      <TextField fullWidth variant="standard" placeholder="(선택 사항)" />
+      <TextField
+        fullWidth
+        variant="standard"
+        placeholder="(선택 사항)"
+        value={company}
+        onChange={(e) => {
+          setCompany(e.target.value);
+        }}
+      />
       <Typography marginTop={2}>3. 이메일*</Typography>
-      <TextField fullWidth variant="standard" placeholder="(필수 사항)" />
+      <TextField
+        fullWidth
+        variant="standard"
+        placeholder="(필수 사항)"
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
       <Typography marginTop={2}>4. 전화번호</Typography>
-      <TextField fullWidth variant="standard" placeholder="(선택 사항)" />
+      <TextField
+        fullWidth
+        variant="standard"
+        placeholder="(선택 사항)"
+        value={phone}
+        onChange={(e) => {
+          setPhone(e.target.value);
+        }}
+      />
       <Typography marginTop={2}>5. 문의 유형*</Typography>
       <FormControl fullWidth sx={{ marginTop: '5px' }}>
         <InputLabel id={'demo-simple-select-label'}>문의 유형</InputLabel>
@@ -62,11 +136,15 @@ export default function ContactForm() {
           <MenuItem value={'이외 기타'}>이외 기타</MenuItem>
         </Select>
       </FormControl>
-      <Typography marginTop={2}>6. 내용*</Typography>
+      <Typography marginTop={2}>6. 내용</Typography>
       <TextField
         multiline
         minRows={5}
         placeholder="내용을 입력해 주세요"
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value);
+        }}
         style={{ width: '100%' }}
       />
       <Box sx={{ display: 'flex', justifyContent: 'center' }}>
@@ -78,6 +156,7 @@ export default function ContactForm() {
             border: '1px solid #c8c8c8',
             color: 'black',
           }}
+          onClick={handleSubmit}
         >
           제출
         </Button>
